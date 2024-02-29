a8ba00336d8ae96ac11070984be1370d6a95f35c
Tofeeq <tofeeq09@gmail.com>
2024-02-29 15:46:20 +0000

    enhancement: add getUsers function and sanatize  post user function response

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,13 +3,30 @@
 const { User } = require("../models");
 
 exports.signupUser = async (req, res) => {
-  console.log(req.body);
   try {
     const { name, handle, email, password } = req.body;
 
     const user = new User({ name, handle, email, password });
     await user.save();
-    res.status(201).send({ user });
+    const { password: userPassword, __v, email: userEmail, ...rest } = user._doc;
+
+    res.status(201).send({ rest });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({ message: error.message });
+  }
+};
+
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+
+    const sanitizedUsers = users.map((user) => {
+      const { password, __v, email, ...rest } = user._doc;
+      return rest;
+    });
+
+    res.status(200).send({ sanitizedUsers });
   } catch (error) {
     console.log(error);
     res.status(400).send({ message: error.message });
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,13 +3,15 @@
 const { Router } = require("express");
 const { userController } = require("../controllers");
 
+// "/users"
 const userRouter = Router();
 
-userRouter.post("/signup", userController.signupUser);
+userRouter.post("/", userController.signupUser);
 // userRouter.post("/login", userController.loginUser);
-// userRouter.get("/users/:id", userController.getUser);
-// userRouter.get("/users/:id/profile", userController.getUserProfile);
-// userRouter.put("/users/:id", userController.updateUser);
-// userRouter.delete("/users/:id", userController.deleteUser);
+userRouter.get("/", userController.getUsers);
+// userRouter.get("/:id", userController.getUser);
+// userRouter.get("/:id/profile", userController.getUserProfile);
+// userRouter.put("/:id", userController.updateUser);
+// userRouter.delete("/:id", userController.deleteUser);
 
 module.exports = userRouter;
