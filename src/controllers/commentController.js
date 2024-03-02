2519d1267cd8eabe50735d032792dd8bfcf03ce0
Tofeeq <tofeeq09@gmail.com>
2024-03-02 10:00:21 +0000

    enhancement: add delete and get comment routes

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -14,7 +14,41 @@ exports.addComment = async (req, res) => {
       content: content,
     });
 
-    res.status(200).json({ message: "Comment added successfully", comment: comment });
+    res.status(200).json({ message: "Comment added successfully", comment });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.getComment = async (req, res) => {
+  try {
+    const { commentId } = req.params;
+
+    const comment = await Comment.findById(commentId).populate("user", "handle name profilePhoto");
+
+    if (!comment) {
+      return res.status(404).json({ message: "Comment not found" });
+    }
+
+    res.status(200).json(comment);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.deleteComment = async (req, res) => {
+  try {
+    const { commentId } = req.params;
+
+    const comment = await Comment.findByIdAndDelete(commentId);
+
+    if (!comment) {
+      return res.status(404).json({ message: "Comment not found" });
+    }
+
+    res.status(200).json({ message: "Comment deleted successfully" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: error.message });
--- src/routes/commentRoutes.js
+++ src/routes/commentRoutes.js
@@ -10,4 +10,10 @@ const commentRouter = Router();
 
 commentRouter.post("/post/:postId", authenticate.authenticate, commentController.addComment);
 
+commentRouter.get("/:commentId", commentController.getComment);
+
+// commentRouter.put("/:commentId", authenticate.authenticate, commentController.updateComment);
+
+commentRouter.delete("/:commentId", authenticate.authenticate, commentController.deleteComment);
+
 module.exports = commentRouter;
